b3e55893e4b343809ebb8d3f54fa792a947305ee
Alyson Souto <alysonartist@gmail.com>
2026-09-10 18:43:56 -0300

    chore: Estrutura inicial do projeto devtask com React e Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+// import Contador from './components/Contador'
+import Tarefa from './components/Tarefa'
+
+const App = () => {
+
+  return (
+    <>
+      {/* <Contador/> */}
+      <Tarefa/>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/Tarefa.jsx
@@ -0,0 +1,6 @@
+
+
+import '../css/estilo.css'
+
+
+
